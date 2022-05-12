040ab47c271956bfcebe6a593f2cdfcbfef2a7b8
Davi Leite <deussecreadger@gmail.com>
2022-05-12 18:17:07 -0300

    Form layout

--- frontend/src/componentes/Navbar/index.tsx
+++ frontend/src/componentes/Navbar/index.tsx
@@ -11,7 +11,7 @@ function Navbar() {
                     <a href="https://github.com/Dvx777">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
-                            <p className="dsmovie-contact-link">/devsuperior</p>
+                            <p className="dsmovie-contact-link">/Davi Leite</p>
 
                         </div>
                     </a>
